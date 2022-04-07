52ae35b3f96defbdce3a54bf706e54157b235614
Longbot <444693295@qq.com>
2022-04-07 14:59:13 +0800

    Update start print buziness (#6)

--- SACP/business/models/GcodeFileInfo.ts
+++ SACP/business/models/GcodeFileInfo.ts
@@ -1,18 +1,22 @@
 import { readString, stringToBuffer } from '../../helper';
 import { Serializable } from '../../Serializable';
+import {buffer} from "stream/consumers";
 
 export default class GcodeFileInfo implements Serializable {
     md5: string;
 
     gcodeName: string;
 
-    constructor(md5: string = '', gcodeName: string = '') {
+    headType: number;
+
+    constructor(md5: string = '', gcodeName: string = '', headType: number = 0) {
         this.md5 = md5;
         this.gcodeName = gcodeName;
+        this.headType = headType;
     }
 
     toBuffer(): Buffer {
-        return Buffer.concat([stringToBuffer(this.md5), stringToBuffer(this.gcodeName)]);
+        return Buffer.concat([stringToBuffer(this.md5), stringToBuffer(this.gcodeName), Buffer.alloc(1,this.headType)]);
     }
 
     fromBuffer(buffer: Buffer) {
