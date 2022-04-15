3bdf7bedbcbe291e0a9632d29346375863582171
bbaa77770 <444693295@qq.com>
2022-04-15 14:32:39 +0800

    Improvement: Change 3dp models

--- SACP/business/ScreenBusiness.ts
+++
@@ -1,7 +0,0 @@
-import Business from './Business';
-
-export default class ScreenBusiness extends Business {
-    constructor(type: string, socket: any) {
-        super(type, socket);
-    }
-}
--- SACP/business/SerialBusiness.ts
+++
@@ -1,7 +0,0 @@
-import Business from './Business';
-
-export default class SerialBusiness extends Business {
-    constructor(type: string, socket: any) {
-        super(type, socket);
-    }
-}
